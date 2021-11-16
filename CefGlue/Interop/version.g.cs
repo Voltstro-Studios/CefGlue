ba810c2ba8888dd4e617a533fb6260ef56aba185
Voltstro <me@voltstro.dev>
2021-11-16 15:33:40 +1000

    Upgrade to 95.7.18+g0d6005e+chromium-95.0.4638.69
    Support .NET 6

--- CefGlue/Interop/version.g.cs
+++ CefGlue/Interop/version.g.cs
@@ -12,15 +12,15 @@ namespace Xilium.CefGlue.Interop
 {
     internal static unsafe partial class libcef
     {
-        public const string CEF_VERSION = "95.7.10+g00d4ad5+chromium-95.0.4638.54";
+        public const string CEF_VERSION = "95.7.18+g0d6005e+chromium-95.0.4638.69";
         public const int CEF_VERSION_MAJOR = 95;
-        public const int CEF_COMMIT_NUMBER = 2452;
-        public const string CEF_COMMIT_HASH = "00d4ad546e8e9571991b4ecc4d7cc28a4d8836cf";
+        public const int CEF_COMMIT_NUMBER = 2460;
+        public const string CEF_COMMIT_HASH = "0d6005e731a64d870868b98c5cba6ca96c42ca3b";
         
         public const int CHROME_VERSION_MAJOR = 95;
         public const int CHROME_VERSION_MINOR = 0;
         public const int CHROME_VERSION_BUILD = 4638;
-        public const int CHROME_VERSION_PATCH = 54;
+        public const int CHROME_VERSION_PATCH = 69;
         
         public const string CEF_API_HASH_UNIVERSAL = "21ac25aebdb49a8e8088c6fbee802b04fd07b501";
         
