4e5838a3d55b45037bf8cccbc31f0368d697a847
Voltstro <me@voltstro.dev>
2022-04-12 23:44:31 +1000

    Fix release build

--- CefGlue/Interop/Base/cef_string_t.cs
+++ CefGlue/Interop/Base/cef_string_t.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
+using System.Security;
 
 namespace Xilium.CefGlue.Interop;
 
--- CefGlue/Interop/libcef.cs
+++ CefGlue/Interop/libcef.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using System.Security;
 
 namespace Xilium.CefGlue.Interop;
 
